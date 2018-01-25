d44f086ed04d856f607134a85bd646d9b001c9aa
Aman <amangupta16@live.com>
2018-01-25 16:10:48 -0600

    Input city

--- src/WeatherExcercise.java
+++ src/WeatherExcercise.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * 
  */
@@ -14,6 +14,11 @@ public class WeatherExcercise {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Where are you?");
+        String cityName = sc.nextLine();
+        System.out.println(cityName);
 
 	}
 
